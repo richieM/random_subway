c9ba6ee3325809d91cf92f41cf39e08eea04a827
Richie Mendelsohn <richiem@apple.com>
2025-09-15 20:41:07 -0400

    get_gtfs stuff

---
+++ gtfs_subway/get_gtfs_subway.py
@@ -0,0 +1,6 @@
+## Download zip from https://rrgtfsfeeds.s3.amazonaws.com/gtfs_subway.zip
+## And process it
+class Get_GTFS_Subway:
+    def download_gtfs():
+        # https://rrgtfsfeeds.s3.amazonaws.com/gtfs_subway.zip
+        pass
---
+++ processing/process_gtfs.py
@@ -0,0 +1,2 @@
+class Process_GTFS:
+    pass
